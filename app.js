5dc9a32be587fd5a7f5902ad05091602919872a7
shreya jain <jainshreya.879@gmail.com>
2017-02-24 23:17:27 +0530

    changes

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var app=express();
 app.get("/", function(req,res) {
 res.send("Hello World!");
 });
-var port = process.env.["PORT"] || 3000;
+var port = process.env["PORT"] || 3000;
 app.listen(port);
